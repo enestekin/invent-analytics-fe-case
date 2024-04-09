c59f1f50b25604a3e673468d82163447b646abea
enestekin <zenestekin@gmail.com>
2024-04-09 16:17:33 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const fecthData = async () => {
+  //   const response = await fetch(
+  //     'http://www.omdbapi.com/?s=pokemon&apikey=db95e598'
+  //   );
+  //   const responseData = await response.json();
+  //   console.log(responseData);
+  // };
+
+  // useEffect(() => {
+  //   fecthData();
+  // }, []);
+
+  return <div>App component</div>;
 }
 
 export default App;
